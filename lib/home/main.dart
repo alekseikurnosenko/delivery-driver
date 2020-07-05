bdb890dfdb91e573bc381259102e65fbed085092
Aleksey Kurnosenko <lekz112@gmail.com>
2020-07-05 23:21:39 +0200

    Integrate function_widget package

--- lib/home/main.dart
+++ lib/home/main.dart
@@ -1,10 +1,57 @@
 import 'dart:async';
 import 'package:delivery_driver/profile/main.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:openapi/api.dart' as API;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'earnings.dart';
+
+part 'main.g.dart';
+
+@widget
+Widget topPanel() => Container(
+    margin: EdgeInsets.only(top: 16),
+    child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      Expanded(flex: 1, child: Container()),
+      Expanded(
+        flex: 1,
+        child: Container(
+            margin: EdgeInsets.only(top: 0),
+            child: Center(child: EarningsPill())),
+      ),
+      Expanded(
+          flex: 1,
+          child: Container(
+              margin: EdgeInsets.only(right: 16),
+              alignment: Alignment.centerRight,
+              child: ProfileWidget()))
+    ]));
+
+@widget
+Widget bottomPanel(BuildContext context) => Column(children: [
+      Container(
+        margin: EdgeInsets.only(bottom: 16),
+        child: FloatingActionButton.extended(
+            label: Text("Go online"),
+            onPressed: () async {
+              // TODO: get valid courier token by running SIM.
+              API.CouriersApi courierAPI = API.CouriersApi();
+              API.Courier courier = await courierAPI.ownCourier();
+              print(courier.fullName);
+            }),
+      ),
+      Container(
+        height: 56,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16), topRight: Radius.circular(16))),
+        alignment: Alignment.center,
+        child: Text("You are offline",
+            style: Theme.of(context).textTheme.headline6),
+      )
+    ]);
 
 class HomePage extends StatelessWidget {
   @override
@@ -13,50 +60,10 @@ class HomePage extends StatelessWidget {
         child: Stack(children: [
       MapWidget(),
       Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        Container(
-            margin: EdgeInsets.only(top: 16),
-            child:
-                Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-              Expanded(flex: 1, child: Container()),
-              Expanded(
-                flex: 1,
-                child: Container(
-                    margin: EdgeInsets.only(top: 0),
-                    child: Center(child: EarningsPill())),
-              ),
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                      margin: EdgeInsets.only(right: 16),
-                      alignment: Alignment.centerRight,
-                      child: ProfileWidget()))
-            ])),
+        TopPanel(),
         Expanded(child: Container()),
-        Column(children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 16),
-            child: FloatingActionButton.extended(
-                label: Text("Go online"),
-                onPressed: () async {
-                  // TODO: get valid courier token by running SIM.
-                  API.CouriersApi courierAPI = API.CouriersApi();
-                  API.Courier courier = await courierAPI.ownCourier();
-                  print(courier.fullName);
-                }),
-          ),
-          Container(
-            height: 56,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16),
-                    topRight: Radius.circular(16))),
-            alignment: Alignment.center,
-            child: Text("You are offline",
-                style: Theme.of(context).textTheme.headline6),
-          )
-        ])
-      ])
+        BottomPanel()
+      ]),
     ]));
   }
 }
---
+++ lib/home/main.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class TopPanel extends StatelessWidget {
+  const TopPanel({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => topPanel();
+}
+
+class BottomPanel extends StatelessWidget {
+  const BottomPanel({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => bottomPanel(_context);
+}
